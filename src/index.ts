0844faca8b0c317a44216d25e52693270511dab6
Pcjmy <1716045191@qq.com>
2023-03-06 19:12:13 +0800

    feat: 环境搭建

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+console.log('abc');
+let count: number = 23;
+console.log('count: ', count);
---
+++ src/shop/shop.ts
@@ -0,0 +1,6 @@
+console.log('Hello World');
+
+let count: number = 2;
+console.log('count: ', count);
+
+export { }
