108a4aca3694404a12dd7d7c34f4ebbcac4b8f73
Pcjmy <1716045191@qq.com>
2023-04-09 14:49:51 +0800

    feat: 手写call

--- src/context/call.test.ts
+++ src/context/call.test.ts
@@ -14,7 +14,19 @@ describe('自定义 call', () => {
       return this
     }
     // @ts-ignore
-    const res = fn.MyCall('abc')
-    expect(res.toString()).toBe('abc')
+    const res1 = fn.MyCall('abc')
+    expect(res1.toString()).toBe('abc')
+
+    // @ts-ignore
+    const res2 = fn.MyCall(null)
+    expect(res2).not.toBeNull()
+  })
+  it('绑定参数', () => {
+    function fn(a: number, b: number, c: number) {
+      return a + b
+    }
+    // @ts-ignore
+    const res = fn.MyCall(null, 10, 20)
+    expect(res).toBe(30)
   })
 })
